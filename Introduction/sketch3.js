250d22a3937b474eca82abbd9c8ecb8169549b10
Emily97 <N00150623@student.iadt.ie>
2018-01-17 14:07:58 +0000

    Sketch 3 comments added

--- Introduction/sketch3.js
+++ Introduction/sketch3.js
@@ -8,19 +8,21 @@ function setup() {						//In our setup we create a canvas. The loop populates th
 	}
 }
 
-function draw() {
-	background(127);
-	var index = floor(random(total));
-	randomCounts[index]++;
-
+function draw() {						//So now everytime we draw (or the draw loop occurs each frame) we
+	background(127);					//redraw the background(see what would happen if you didn't do this)
+	var index = floor(random(total));	//We then choose a random number between 0 and 20, floor it( so the results will be 0-19 integers)
+	randomCounts[index]++;				//and we add it to the value in the array. Example 6.76 randomly chosen - floored to 6
+										//-randomCounts[6] = 0 + 1 = 1 [0,0,0,0,0,1,0,0,0,0,0,0,0,0]
 	//Draw a rectangle to graph results
-	stroke(0);
-	strokeWeight(2);
+	stroke(0);							//Black stroke with weight of 2px and fill of white selected and ready to be 
+	strokeWeight(2);					//used to draw something. Calculates the width of each bar
 	fill(255);
 
 	var w = width/randomCounts.length;
 
 	for (var x = 0; x < randomCounts.length; x++) {
 		rect(x*w, height - randomCounts[x], w-1, randomCounts[x]);
+		//Loops 20 times(once for each bar) and creates a rectangle with a height
+		//based on the array value at the time. Rect(x Start Pos, y Start Pos, width, height)
 	}
 }
